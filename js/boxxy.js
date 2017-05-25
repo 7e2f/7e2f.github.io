eb584c03d00e9ba7734f3b1bd278e3374e5a7d27
kittiecat <astranomaly@gmail.com>
2017-05-24 20:58:26 -0400

    update

--- js/boxxy.js
+++ js/boxxy.js
@@ -10,35 +10,55 @@ $(document).ready(function() {
 
     for(i in linkArray) {
       var line = jQuery.trim(linkArray[i]);
+      var lineType = line.substr(0,1)
 
       if(!line) {
         continue;
-      } else if(line.substr(0,1) == '&') {
-        if(column > 1) {
-          html = html + '</ul></div></div>';
-        }
-        var lineArray = line.substr(2).split(' | ');
-        var cols = lineArray[0];
-        linkbox = 1;
-        column = column + 1;
-        html = html + '<div class="column col-' + cols + '">';
-        continue;
-      } else if(line.substr(0,1) == '#') {
-        if(linkbox > 1) {
-          html = html + '</ul></div>';
-        }
-        linkbox = linkbox + 1;
-        html = html + '<div class="box"><h1>' + line.substr(2) + '</h1><ul><br/>';
-        continue;
-      } else if(line.substr(0,1) == '@') {
-        var lineArray = line.substr(2).split(' | ');
-        var url = lineArray[0];
-        var title = lineArray[1];
-        html = html + '[<a href="' + url + '"target="_blank">' + title + '</a>]<br>'
-      } else {
-        continue;
-      }
-    }
+      } else switch (lineType) {
+        case "&":
+          if(column > 1) { html += '</ul></div></div>'; };
+          var splitLine = line.substr(2).split(' | ');
+          var columnWidth = splitLine[0];
+          linkbox = 1;
+          column += 1;
+          html += '<div class="column col-' + columnWidth + '">';
+          continue;
+        case "#":
+          if(linkbox > 1) { html += '</ul></div>'; }
+          linkbox += 1
+          html += '<div class="box"><h1>' + line.substr(2) + '</h1><ul><br/>';
+          continue;
+        case "@":
+          var splitLine = line.substr(2).split(' | ');
+          var url = splitLine[0];
+          var title = splitLine[1];
+          html += '[<a href="' + url + '"target="_blank">' + title + '</a>]<br>';
+          continue;
+        case "?":
+          // continue;
+          var temp = localStorage.getItem("savedStorage");
+          if(temp) {
+            if(linkbox > 1) { html += '</ul></div>'; }
+            linkbox += 1
+            html += '<div class="box"><h1>temp [<a href="wip-cache/"target="_blank"> &gt </a>]<br></h1><ul><br/>';
+            var tempLinkArray = temp.split("\n");
+            for(i in tempLinkArray) {
+              var line = jQuery.trim(tempLinkArray[i]);
+              var lineType = line.substr(0,1)
+              if(!line) {
+                continue;
+              } else {
+                  var splitLine = line.substr(2).split(' | ');
+                  var url = splitLine[0];
+                  var title = splitLine[1];
+                  html += '[<a href="' + url + '"target="_blank">' + title + '</a>]<br>';
+                  continue;
+                }};
+          };
+        default:
+          continue;
+      };
+    };
 
     html = html + '</ul></div></div>';
     $('boxview').append(html);
